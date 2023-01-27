96c1f42bd4963b05efc61b4bcb6f1e1096a82696
Elnar Mengelbaev <elnar_men@mail.ru>
2023-01-27 17:36:29 +0300

    add display of place-related images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ['image', 'get_image_preview']
+    readonly_fields = ['get_image_preview']
+
+    def get_image_preview(self, obj):
+        width = 150,
+        height = 150
+        return format_html(
+            "{}",
+            mark_safe(
+                f'<img src="{obj.image.url}" width="{width}" height={height} />'
+            )
+        )
 
 
 @admin.register(Place)
--- places/models.py
+++ places/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=255)
-    description_short = models.TextField()
-    description_long = models.TextField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    title = models.CharField('Компания', max_length=255)
+    description_short = models.TextField('Краткое описание')
+    description_long = models.TextField('Подробное описание')
+    longitude = models.FloatField('Долгота')
+    latitude = models.FloatField('Широта')
 
 
     def __str__(self):
@@ -17,13 +17,18 @@ class Image(models.Model):
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
-        related_name='images'
+        related_name='images',
+        verbose_name='Компания'
     )
-    title = models.CharField(max_length=200)
-    image = models.ImageField()
+    title = models.CharField(max_length=200, verbose_name='Компания')
+    image = models.ImageField(verbose_name='Картинка')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
 
 
 
