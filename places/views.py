e89876b0433cf3b62616cc13a3e3cbb91ce9b8c0
Elnar Mengelbaev <elnar_men@mail.ru>
2023-01-27 15:34:47 +0300

    replace test data with data from data base

--- places/views.py
+++ places/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http.response import JsonResponse
 from .models import Place
+from django.urls import reverse
+
+
+def get_place_details(place):
+
+    img_urls = [image.image.url for image in place.images.all()]
+
+    place_details = {
+        'title': place.title,
+        'imgs': img_urls,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
+        }
+    }
+
+    return place_details
 
 
 def show_map(request):
@@ -21,14 +41,26 @@ def show_map(request):
                 },
                 "properties": {
                     "title": place.title,
-                    "placeId": place.pk,
-                    "detailsUrl": "/static/places/moscow_legends.json"
+                    "placeId": place.id,
+                    "detailsUrl": reverse(
+                        'show_place', kwargs={'place_id': place.pk}
+                    )
                 }
             }
         )
 
     places_geojson = {'places': places}
 
-    return render(request, 'index.html', context=places_geojson)
+    return render(request, 'places/index.html', context=places_geojson)
 
+
+def show_place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    place_details = get_place_details(place)
+
+    return JsonResponse(
+        place_details,
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_map)
+    path('', views.show_map),
+    path('places/<int:place_id>/', views.show_place, name='show_place')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
