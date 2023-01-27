8152d3c712f97f380e6b1392abfc257ce8020a24
Elnar Mengelbaev <elnar_men@mail.ru>
2023-01-27 10:17:47 +0300

    get list of locations from database

--- places/models.py
+++ places/models.py
@@ -5,8 +5,8 @@ class Place(models.Model):
     title = models.CharField(max_length=255)
     description_short = models.TextField()
     description_long = models.TextField()
-    longitude = models.DecimalField(max_digits=16, decimal_places=14)
-    latitude = models.DecimalField(max_digits=16, decimal_places=14)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
 
 
     def __str__(self):
--- places/views.py
+++ places/views.py
@@ -1,39 +1,34 @@
 from django.shortcuts import render
+from .models import Place
 
 
 def show_map(request):
 
-    data = {
-      'json':
-        {
-          "type": "FeatureCollection",
-          "features": [
-            {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [37.62, 55.793676]
-              },
-              "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": "static/places/moscow_legends.json"
-              }
-            },
-            {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [37.64, 55.753676]
-              },
-              "properties": {
-                "title": "Крыши24.рф",
-                "placeId": "roofs24",
-                "detailsUrl": "static/places/roofs24.json"
-              }
-            }
-          ]
-        }
+    places_query_set = Place.objects.all()
+
+    places = {
+      "type": "FeatureCollection",
+      "features": []
     }
 
-    return render(request, 'index.html', context=data)
+    for place in places_query_set:
+        places["features"].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.longitude, place.latitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": "/static/places/moscow_legends.json"
+                }
+            }
+        )
+
+    places_geojson = {'places': places}
+
+    return render(request, 'index.html', context=places_geojson)
+
+
